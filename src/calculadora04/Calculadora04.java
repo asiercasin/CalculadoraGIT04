f024c309281cb49fc35e00abede07992a539431b
Asier <asier.casin07@somo.eus>
2026-02-05 13:30:18 +0100

    cambio en dividir: manejo de error sin excepcion

--- src/calculadora04/Calculadora04.java
+++ src/calculadora04/Calculadora04.java
@@ -13,10 +13,10 @@ public class Calculadora04 {
 	}
 	public double dividir(int a, int b) {
 		 if (b == 0) {
-		 throw new IllegalArgumentException("No se puede dividir entre cero");
+		 System.out.println("Error: división entre cero");
+		 return 0;
 		 }
 		 return (double) a / b;
 		}
- 
-
+		 
 }
