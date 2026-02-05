792d08db95c815bcd6a8e715572ce454c26e1ee8
Asier <asier.casin07@somo.eus>
2026-02-05 13:27:04 +0100

    adicion de la potencia y modulo

--- src/calculadora04/Calculadora04.java
+++ src/calculadora04/Calculadora04.java
@@ -16,7 +16,17 @@ public class Calculadora04 {
 		 throw new IllegalArgumentException("No se puede dividir entre cero");
 		 }
 		 return (double) a / b;
-		}
+	}
  
+	public int potencia(int base, int exponente) {
+		 int resultado = 1;
+		 for (int i = 0; i < exponente; i++) {
+		 resultado *= base;
+	}
+		 return resultado;
+	}
+	public int modulo(int a, int b) {
+		 return a % b;
+	}
 
 }
